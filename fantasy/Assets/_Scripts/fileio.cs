878ca08e537b521a1cc3262c3e0a5f27ef02ca42
Rachel Vancleave <sindiewen@gmail.com>
2020-07-10 15:17:45 -0700

    pushing fileio test

---
+++ fantasy/Assets/_Scripts/fileio.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+
+public class fileio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogError(Application.dataPath + "/../helloworld");
+        string path = Application.dataPath + "/../helloworld";
+
+        if (!File.Exists(path))
+        {
+            // Create a file to write to.
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine("Hello");
+                sw.WriteLine("And");
+                sw.WriteLine("Welcome");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
