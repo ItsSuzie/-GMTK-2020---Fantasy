ce2937e099b25189595d22460cf6a38007a55a49
Lyncht98 <tadhglynch4@gmail.com>
2020-07-11 02:23:57 -0400

    A very bad missile firing system
    going to bed, but need to write the missile prefab to look for the player so that it doesn't need to start as a gameobject

---
+++ fantasy/Assets/SpawningMissiles.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningMissiles : MonoBehaviour
+{
+
+    public GameObject prefabToSpawn;
+    public float spawnTime;
+    public float spawnTimeRandom;
+
+    private float spawnTimer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetSpawnTimer();
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0.0f)
+        {
+            Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+            ResetSpawnTimer();
+        }
+    }
+
+    //Resets the spawn timer with a random offset
+    void ResetSpawnTimer()
+    {
+        spawnTimer = spawnTime;
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ fantasy/Assets/explode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explode : MonoBehaviour
+{
+
+    private BoxCollider2D boxCollider;
+    private LayerMask player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        player = LayerMask.GetMask("player");
+    }
+
+// Update is called once per frame
+void Update()
+    {
+
+        Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0, player);
+
+        foreach (Collider2D hit in hits)
+        {
+            ColliderDistance2D colliderDistance = hit.Distance(boxCollider);
+
+            if (colliderDistance.isOverlapped)
+            {
+                Destroy(gameObject);
+            }
+
+        }
+    }
+}
